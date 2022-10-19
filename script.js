1eec17fbe864e0931d21735f08f2a01a2c73ba81
Luke Lai <lukelai.dev@gmail.com>
2022-10-19 12:37:36 +0100

    add preview image

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
 const animateIn = (element, remove, add) => {
   element.classList.remove(remove);
@@ -18,24 +19,20 @@ const toggleNav = () => {
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
     // Animate In
-    // overlay.classList.remove("overlay-slide-left");
-    // overlay.classList.add("overlay-slide-right");
-    animateIn(nav1, "slide-out-1", "slide-in-1");
-    animateIn(nav2, "slide-out-2", "slide-in-2");
-    animateIn(nav3, "slide-out-3", "slide-in-3");
-    animateIn(nav4, "slide-out-4", "slide-in-4");
-    animateIn(nav5, "slide-out-5", "slide-in-5");
+    animateIn(overlay, "overlay-slide-left", "overlay-slide-right");
+    navItems.forEach((navItem, index) =>
+      animateIn(navItem, `slide-out-${index + 1}`, `slide-in-${index + 1}`)
+    );
+
     // Nav Items
     animateIn(overlay, "overlay-slide-left", "overlay-slide-right");
   } else {
     // overlay.classList.remove("overlay-slide-right");
     // overlay.classList.add("overlay-slide-left");
     animateIn(overlay, "overlay-slide-right", "overlay-slide-left");
-    animateIn(nav1, "slide-in-1", "slide-out-1");
-    animateIn(nav2, "slide-in-2", "slide-out-2");
-    animateIn(nav3, "slide-in-3", "slide-out-3");
-    animateIn(nav4, "slide-in-4", "slide-out-4");
-    animateIn(nav5, "slide-in-5", "slide-out-5");
+    navItems.forEach((navItem, index) =>
+      animateIn(navItem, `slide-in-${index + 1}`, `slide-out-${index + 1}`)
+    );
   }
 };
 
